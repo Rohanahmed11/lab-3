d180dc6b5b35ce7592fe96753c322fa99d7c3572
Rohanahmed11 <roah4500@gmail.com>
2024-10-31 16:20:56 +0000

    Update Agent.java

--- Task2/Agent.java
+++ Task2/Agent.java
@@ -1,5 +1,4 @@
 package Task2;
-//Class representing an Agent that implements common functions
 class Agent implements CommonFunctions {
  private int id;
  private String name;
@@ -8,27 +7,22 @@ class Agent implements CommonFunctions {
      this.id = id;
      this.name = name;
  }
-
  @Override
  public void searchTicket() {
      System.out.println("Searching for tickets...");
  }
-
  @Override
  public void bookTicket() {
      System.out.println("Booking a ticket...");
  }
-
  @Override
  public void cancelTicket() {
      System.out.println("Cancelling a ticket...");
  }
-
  @Override
  public void makePayment() {
      System.out.println("Making payment...");
  }
-
  @Override
  public void fillDetails() {
      System.out.println("Filling details...");
