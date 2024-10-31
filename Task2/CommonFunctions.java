f5db4226f19cfa45bbb4fd47d3a0d4edb92f2550
Rohanahmed11 <roah4500@gmail.com>
2024-10-31 16:17:59 +0000

    Update CommonFunctions.java

--- Task2/CommonFunctions.java
+++ Task2/CommonFunctions.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-// Interface for common functions related to ticket booking
 interface CommonFunctions {
     void searchTicket();
     void bookTicket();
