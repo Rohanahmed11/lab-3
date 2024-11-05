2879722234f4853ce1c8b3ec9d96173dda4e6908
Rohanahmed11 <roah4500@gmail.com>
2024-11-05 19:59:57 +0000

    Update BookingCounter.java

--- Task2/BookingCounter.java
+++ Task2/BookingCounter.java
@@ -1,20 +1,3 @@
 package Task2;
-
-import java.util.ArrayList;
-import java.util.List;
-
-class BookingCounter {
- private List<Ticket> bookedTickets;
-
- public BookingCounter() {
-     bookedTickets = new ArrayList<>(); 
- }
-
- public void addBookedTicket(Ticket ticket) {
-     bookedTickets.add(ticket);
- }
-
- public List<Ticket> getBookedTickets() {
-     return bookedTickets;
- }
+public class BookingCounter {
 }
