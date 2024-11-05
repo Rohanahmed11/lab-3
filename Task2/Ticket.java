c202ed436cce28fb7b5130a9c68044cff4f5b3ff
Rohanahmed11 <roah4500@gmail.com>
2024-11-05 20:11:17 +0000

    Update Ticket.java

--- Task2/Ticket.java
+++ Task2/Ticket.java
@@ -1,32 +1,66 @@
 package Task2;
 import java.util.Date;
-class Ticket {
- private String source;
- private String destination;
- private Date dateOfJourney;
- private String time;
- private String busNo;
- private String seatNo;
+public class Ticket {
+    private String source;
+    private String destination;
+    private Date dateOfJourney;
+    private String time;
+    private String busNo;
+    private String seatNo;
 
- public Ticket(String source, String destination, 
- Date dateOfJourney, String time, String busNo, String seatNo) {
-     this.source = source;
-     this.destination = destination;
-     this.dateOfJourney = dateOfJourney;
-     this.time = time;
-     this.busNo = busNo;
-     this.seatNo = seatNo;
- }
+    public Ticket(String source, String destination, 
+    Date dateOfJourney, String time, String busNo, String seatNo) {
+        this.source = source;
+        this.destination = destination;
+        this.dateOfJourney = dateOfJourney;
+        this.time = time;
+        this.busNo = busNo;
+        this.seatNo = seatNo;
+    }
 
- @Override
- public String toString() {
-     return "Ticket{" +
-             "source='" + source + '\'' +
-             ", destination='" + destination + '\'' +
-             ", dateOfJourney=" + dateOfJourney +
-             ", time='" + time + '\'' +
-             ", busNo='" + busNo + '\'' +
-             ", seatNo='" + seatNo + '\'' +
-             '}';
- }
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+    public Date getDateOfJourney() {
+        return dateOfJourney;
+    }
+    public void setDateOfJourney(Date dateOfJourney) {
+        this.dateOfJourney = dateOfJourney;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getBusNo() {
+        return busNo;
+    }
+
+    public void setBusNo(String busNo) {
+        this.busNo = busNo;
+    }
+
+    public String getSeatNo() {
+        return seatNo;
+    }
+
+    public void setSeatNo(String seatNo) {
+        this.seatNo = seatNo;
+    }
 }
