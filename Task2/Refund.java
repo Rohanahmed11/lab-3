b052c22585611faa248df3d9c63b51fa718f781d
Rohanahmed11 <roah4500@gmail.com>
2024-11-05 20:13:02 +0000

    Update Refund.java

--- Task2/Refund.java
+++ Task2/Refund.java
@@ -1,15 +1,31 @@
 package Task2;
-class Refund {
- private float amount;
- private String customerId;
+public class Refund {
+    private float amount;
+    private String customerId;
 
- public Refund(float amount, String customerId) {
-     this.amount = amount;
-     this.customerId = customerId;
- }
+    public Refund(float amount, String customerId) {
+        this.amount = amount;
+        this.customerId = customerId;
+    }
 
- public void refundAmount() {
-     System.out.println("Refunding " + amount + 
-      " to customer " + customerId);
- }
+    public void refundAmount() {
+        System.out.println("Processing refund for customer ID " 
+        + customerId + " with amount $" + amount);
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
 }
